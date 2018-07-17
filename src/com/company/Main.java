5802bd1de7442ee73e92c803b7dfbf85da8e3c78
LENGYUANL <lengyuan2236@gmail.com>
2018-07-17 13:50:37 +0800

    testn

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello SVW");
-        System.out.println("hello auto+");
+        System.out.println("hello Auto+");
         System.out.println("hello SVW");
         System.out.println("hello SVW");
         System.out.println("hello World");
         System.out.println("hello World");
-
     }
 }
